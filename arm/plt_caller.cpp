ded19a2383bfb214539bdd84da09ac02c2854607
linzj <linzj@ucweb.com>
2015-01-12 10:45:58 +0800

    * arm/plt_caller.cpp: Update pt_regs defines.

--- arm/plt_caller.cpp
+++ arm/plt_caller.cpp
@@ -6,8 +6,36 @@
 #include <string.h>
 
 plt_caller::plt_caller () {}
+namespace _private
+{
 
-struct user_regs_struct : pt_regs
+struct pt_regs
+{
+  unsigned long uregs[18];
+};
+
+#ifndef ARM_cpsr
+#define ARM_cpsr uregs[16]
+#define ARM_pc uregs[15]
+#define ARM_lr uregs[14]
+#define ARM_sp uregs[13]
+#define ARM_ip uregs[12]
+#define ARM_fp uregs[11]
+#define ARM_r10 uregs[10]
+#define ARM_r9 uregs[9]
+#define ARM_r8 uregs[8]
+#define ARM_r7 uregs[7]
+#define ARM_r6 uregs[6]
+#define ARM_r5 uregs[5]
+#define ARM_r4 uregs[4]
+#define ARM_r3 uregs[3]
+#define ARM_r2 uregs[2]
+#define ARM_r1 uregs[1]
+#define ARM_r0 uregs[0]
+#define ARM_ORIG_r0 uregs[17]
+#endif
+}
+struct user_regs_struct : _private::pt_regs
 {
 };
 
