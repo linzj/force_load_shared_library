880085a2b76af5fa065d6a8902c3647846a34c8e
linzj <manjian2006@gmail.com>
2015-01-21 00:24:12 +0800

    Make android build.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,16 @@ LOCAL_PATH := $(call my-dir)/../
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := plt_caller_test
-LOCAL_SRC_FILES := got_finder.cpp log.cpp ptracer.cpp arm/plt_caller_test.cpp arm/plt_caller.cpp
+ARM_SRC := arm/plt_caller_test.cpp arm/plt_caller.cpp
+I686_SRC := i686/plt_caller_test.cpp i686/plt_caller.cpp
+ifeq ($(TARGET_ARCH), arm)
+TARGET_SRC := $(ARM_SRC)
+else
+ifeq ($(TARGET_ARCH), x86)
+TARGET_SRC := $(I686_SRC)
+endif # x86
+endif # arm
+LOCAL_SRC_FILES := got_finder.cpp log.cpp ptracer.cpp $(TARGET_SRC)
 LOCAL_LDFLAGS = -pie
 LOCAL_LDLIBS += -llog
 
@@ -19,8 +28,18 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
+ARM_SRC := arm/plt_caller.cpp
+I686_SRC := i686/plt_caller.cpp
+ifeq ($(TARGET_ARCH), arm)
+TARGET_SRC := $(ARM_SRC)
+else
+ifeq ($(TARGET_ARCH), x86)
+TARGET_SRC := $(I686_SRC)
+endif # x86
+endif # arm
+
 LOCAL_MODULE    := ldso
-LOCAL_SRC_FILES := got_finder.cpp log.cpp ptracer.cpp arm/plt_caller.cpp main.cpp
+LOCAL_SRC_FILES := got_finder.cpp log.cpp ptracer.cpp main.cpp $(TARGET_SRC)
 LOCAL_LDFLAGS = -pie
 LOCAL_LDLIBS += -llog
 
